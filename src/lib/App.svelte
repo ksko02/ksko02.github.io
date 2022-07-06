9fdea2ddb8aae188b9a67ed5b671fd613bab60aa
ksko02 <74777950+ksko02@users.noreply.github.com>
2022-07-06 12:21:40 +0300

    Add files via upload

---
+++ src/lib/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	let src1 = "https://i.gifer.com/Gjnl.gif";
+	let scr2 = "https://i.gifer.com/14Se.gif";
+	let scr3 = "https://i.gifer.com/1PyX.gif";
+
+</script>
+
+<main>
+	<p align="center">Welcome to this site. This site will tell you about Ksusha's life and hobbies. Now you can read a short story about her life. Have an exciting trip!</p>
+	<img src={src1} alt="" align="left" width="180" height="150" style=" border: 8px solid #DADEDF"/>
+	<p align="center">On December 24, 2002, in the small city of Saratov, a little girl named Oksana was born, but everyone called her Ksusha. In her early years, she had a lot of help from her parents and a cool older sister. They taught her to walk, talk, and read.</p>
+	<img src={scr2} alt="" align="right" width="180" height="150" style=" border: 8px solid #DADEDF"/>
+	<p align="right">She was a very calm and curious child who wanted to conquer the world. As the years passed, Ksusha went to kindergarten, where she met her first friends. Ksusha was 7 years old when the first school bell rang and the carefree childhood with quiet hours and toys ended. It was time to study and learn a lot of things. Her school life was very interesting and fun. Ksusha studied well and liked to learn a lot of new things! Her favorite subject was math.</p>
+	<p align="center"></p>
+	<p align="right" class="p1">In first grade she went to dance for the first time and she wasn't doing well. Ksusha didn't get into the music, and her feet were crooked. But years of hard practice allowed her to achieve a lot and find something she loved. Also in the third grade she went to fencing and practiced for many years, but unfortunately it was very difficult for her. When Ksusha was moving into third grade, she had a little sister who was a little loud, but very sweet.</p>
+	<p align="center"></p>
+	<p align="center">Before she knew it, Ksusha was in the 11th grade and was taking some serious exams. Ksusha successful passed the selection process for the wonderful Innopolis University! She has been living and studying at this university for almost a year now.</p>
+	<p align="center">Unfortunately, I can't tell this fascinating story any further, because Ksusha herself doesn't know yet what adventures await her ahead!</p>
+	<img src={scr3} alt="" align="center" width="180" height="150" class="im1" id="heart"/>
+</main>
+
+<style>
+	
+	main{
+		background-color:#DADEDF
+	} 
+
+	#heart{
+		margin: 0 auto;
+		display: block;
+
+	}
+
+
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from "$lib/Header.svelte";
+	import About from "$lib/App.svelte";
+	
+    let condition: number = 1;
+</script>
+
+
+<main>
+	<Header {condition}/>
+    <About />
+    
+</main>
+
+
+<style>
+</style>
